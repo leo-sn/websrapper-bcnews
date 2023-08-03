71e7c7c268ee021fce3bd64d47a529b6f2da1cf0
Leo Nascimento <95266043+leo-sn@users.noreply.github.com>
2023-08-02 20:50:34 -0700

    created initial structure to scrape

--- scrape.js
+++ scrape.js
@@ -1,1 +1,41 @@
-//this is a start file!
+import puppeteer from 'puppeteer';
+
+const url = "https://www.richmond-news.com/highlights";
+
+//Create scrapping function
+
+const main = async () => {
+    // launch a new browser using puppeteer
+    const browser = await puppeteer.launch({headless: false});
+    // create new page using puppeteer
+    const page = await browser.newPage();
+    // go to the webpage
+    await page.goto(url);
+   
+    // load the whole page with evaluate
+    const allNews = await page.evaluate(() => {
+        //create the selector from the page
+        const newsSelector = document.getElementsByClassName('media');
+
+        let news = Array.from(newsSelector).slice(0, 3).map((n) => {
+
+
+            const title = n.querySelector('h2').innerText;
+            const date = n.querySelector('time').title;
+            const summary = n.getElementsByClassName('media-intro')[0].innerHTML;
+
+            return {title, summary, date}
+        })
+
+        return (news)
+    })
+
+
+    console.log(allNews)
+
+    // close the browser we created after captured the screenshot
+    await browser.close();
+
+}
+
+main();
